ae8fbec85d2d344691f18419f550eb6b083b2b43
Walle1997 <farid-keltuzed@mail.ru>
2024-10-02 14:06:54 +0300

    .

---
+++ 3.8.js
@@ -0,0 +1,13 @@
+function circleLength(d) {
+  let i;
+  i = d * Math.PI;
+  return i;
+}
+window.document.write('<tr><td>2</td>');
+window.document.write('<td>', circleLength(2), '</td></tr>');
+window.document.write('<tr><td>5</td>');
+window.document.write('<td>', circleLength(5), '</td></tr>');
+window.document.write('<tr><td>10</td>');
+window.document.write('<td>', circleLength(10), '</td></tr>');
+window.document.write('<tr><td>20</td>');
+window.document.write('<td>', circleLength(20), '</td></tr>');
